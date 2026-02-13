c824ba8a149b5919df8579c5228136cdcd89d51e
Hanneke Hogenkamp <167975621+hogenkamph@users.noreply.github.com>
2026-02-13 11:20:18 +0100

    Update shapefile loading script for February 2026
    Drop statement toegevoegd (indien tabel bestaat wordt deze eerst verwijderd) en verwijzingen naar nieuwste bronbestanden.

--- nwb-buitenland/01_Shapebestanden_inladen.bat
+++ nwb-buitenland/01_Shapebestanden_inladen.bat
@@ -8,14 +8,14 @@ SET PGPASSWORD=postgres
 SET POSTGRES_BIN=c:\Program Files\PostgreSQL\15\bin
 
 REM ############################################## DATA INLADEN ############################################################################################
-REM REM NWB
-shp2pgsql -I -D -S -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\01_NWB\januari_2026\Wegvakken.shp" nwb | psql
-REM REM Vlaanderen
-shp2pgsql -I -D -S -s 31370:28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\02_Vlaams_wegenregister\januari_2026\Wegsegment.shp" vlaanderen | psql
+REM NWB
+shp2pgsql -d -I -D -S -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\01_NWB\februari_2026\Wegvakken.shp" nwb | psql
+REM Vlaanderen
+shp2pgsql -d -I -D -S -s 31370:28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\02_Vlaams_wegenregister\februari_2026\Wegsegment.shp" vlaanderen | psql
 REM Studiegebied
-shp2pgsql -I -D -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\04_Studiegebied\Studiegebied.shp" studiegebied | psql
+shp2pgsql -d -I -D -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\04_Studiegebied\Studiegebied.shp" studiegebied | psql
 REM Grensovergangen 
-shp2pgsql -I -D -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\05_Grensovergangen\Grensovergangen.shp" grensovergangen | psql
+shp2pgsql -d -I -D -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\05_Grensovergangen\Grensovergangen.shp" grensovergangen | psql
 REM Uitsluitingen
-shp2pgsql -I -D -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\06_Uitsluitingen\Uitsluitingen.shp" Uitsluitingen | psql
+shp2pgsql -d -I -D -s 28992  "d:\Projects\022054_2025_NWB_Buitenland\Data\06_Uitsluitingen\Uitsluitingen.shp" Uitsluitingen | psql
 pause
